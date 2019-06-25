c299feed7221a99e764472f6e7944c4819c5cce0
Lucas <csalucasnascimento@gmail.com>
2019-06-25 12:20:18 +1000

    cotroller folder

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { PORT } from './config';
 
 import App from './App';
 
-import AssetsController from './assets/asset.controller';
+import AssetsController from './controllers/asset.controller';
 
 const app = new App([new AssetsController()], PORT, '0.0.0.0');
 
