a4293f2f6d604bc24744b216633c1e9352b6b54a
Arthemus <arthemus.moreira@gmail.com>
2019-07-17 09:32:56 +1000

    Defining authentication middleware.

--- src/config.ts
+++ src/config.ts
@@ -9,3 +9,5 @@ export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 6007;
 export const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
 export const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 export const bucket = process.env.AWS_BUCKET;
+
+export const USERS_AUTHENTICATION_API_HOST = process.env.USERS_API_HOST || "http://localhost:6001";
