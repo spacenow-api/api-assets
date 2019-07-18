f889bac846882983a219436396ba4a4229c47a98
Lucas <csalucasnascimento@gmail.com>
2019-07-18 10:28:13 +1000

    changing version sharp

--- src/helpers/middlewares/auth-middleware.ts
+++ src/helpers/middlewares/auth-middleware.ts
@@ -1,29 +1,31 @@
-import { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import axios from 'axios';
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import axios from "axios";
 
-import AuthenticationTokenMissingException from '../exceptions/AuthenticationTokenMissingException';
-import WrongAuthenticationTokenException from '../exceptions/WrongAuthenticationTokenException';
+import AuthenticationTokenMissingException from "../exceptions/AuthenticationTokenMissingException";
+import WrongAuthenticationTokenException from "../exceptions/WrongAuthenticationTokenException";
 
-import { IUser } from './../../interfaces/user.interface';
+import { IUser } from "./../../interfaces/user.interface";
 
-import Token from '../utils/token';
+import Token from "../utils/token";
 
-import * as config from './../../config';
+import * as config from "./../../config";
 
 const fetchUserById = async (id: string): Promise<string> => {
-  const res = await axios.get(`${config.USERS_AUTHENTICATION_API_HOST}/users/legancy/${id}`);
+  const res = await axios.get(
+    `${config.USERS_AUTHENTICATION_API_HOST}/users/legancy/${id}`
+  );
   if (res && res.data) {
     const userData: IUser = res.data;
     return Promise.resolve(userData.email);
   }
   return Promise.reject();
-}
+};
 
 async function authMiddleware(req: Request, _: Response, next: NextFunction) {
   const token = Token.get(req);
-  if (token && token !== 'undefined') {
-    const secret: string = process.env.JWT_SECRET || 'Spacenow';
+  if (token && token !== "undefined") {
+    const secret: string = process.env.JWT_SECRET || "Spacenow";
     try {
       const { id }: any = await jwt.verify(token, secret);
       const email: string = await fetchUserById(id);
