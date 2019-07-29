8abc6c07e33c7dc5f62848c5ca87243252a89e29
Lucas <csalucasnascimento@gmail.com>
2019-07-29 19:06:23 +1000

    Removing AWS Variables

--- src/services/image.delete.service.ts
+++ src/services/image.delete.service.ts
@@ -1,22 +1,16 @@
-import * as AWS from 'aws-sdk';
-import * as config from '../config';
+import * as AWS from "aws-sdk";
+import * as config from "../config";
 
-const AWS_S3_BUCKET_NAME = config.bucket || '';
+const AWS_S3_BUCKET_NAME = config.bucket || "";
 const s3 = new AWS.S3();
 
-AWS.config.update({
-	secretAccessKey: config.awsSecretAccessKey,
-	accessKeyId: config.awsAccessKeyId,
-	region: 'ap-southeast-2',
-});
-
 const deletePhoto = (Key: string, callback: any) =>
-	s3.deleteObject(
-		{
-			Bucket: AWS_S3_BUCKET_NAME,
-			Key,
-		},
-		callback,
-	);
+  s3.deleteObject(
+    {
+      Bucket: AWS_S3_BUCKET_NAME,
+      Key
+    },
+    callback
+  );
 
 export default deletePhoto;
