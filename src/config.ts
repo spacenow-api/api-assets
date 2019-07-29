378a79909ed06e5af17408276c7a44e485585335
Lucas <csalucasnascimento@gmail.com>
2019-07-29 19:02:44 +1000

    removing AWS Variables

--- src/config.ts
+++ src/config.ts
@@ -6,9 +6,6 @@ export const DEBUG = process.env.DEBUG ? Boolean(process.env.DEBUG) : false;
 export const PORT = process.env.PORT ? parseInt(process.env.PORT) : 6007;
 
 // S3 Bucket
-export const awsAccessKeyId = process.env.AWS_ACCESS_KEY_ID;
-export const awsSecretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-export const awsSessionToken = process.env.AWS_SESSION_TOKEN;
 export const bucket = process.env.S3_BUCKET;
 
 // Database Parameters
--- src/services/image.upload.service.ts
+++ src/services/image.upload.service.ts
@@ -7,13 +7,6 @@ import * as config from "../config";
 const AWS_S3_BUCKET_NAME = config.bucket || "";
 const s3 = new AWS.S3();
 
-AWS.config.update({
-  secretAccessKey: config.awsSecretAccessKey,
-  accessKeyId: config.awsAccessKeyId,
-  sessionToken: config.awsSessionToken,
-  region: "ap-southeast-2"
-});
-
 const imageFilter = (
   request: Express.Request,
   file: Express.Multer.File,
