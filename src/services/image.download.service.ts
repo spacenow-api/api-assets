b3d66a06abcac33f318fa46b391426505e55d4dd
Lucas <csalucasnascimento@gmail.com>
2019-07-29 10:36:41 +1000

    removing path

--- src/services/image.download.service.ts
+++ src/services/image.download.service.ts
@@ -3,11 +3,11 @@ import fs from "fs";
 import * as config from "../config";
 
 const download = async (url: string, photoId: number, callback: any) => {
-  const path = `${config.imagePath}/${photoId}.jpeg`;
-  const output = fs.createWriteStream(path);
-  const response = await axios.get(url, { responseType: "stream" });
-  response.data.pipe(output);
-  response.data.on("end", callback);
+  // const path = `${config.imagePath}/${photoId}.jpeg`;
+  // const output = fs.createWriteStream(path);
+  // const response = await axios.get(url, { responseType: "stream" });
+  // response.data.pipe(output);
+  // response.data.on("end", callback);
 };
 
 export default download;
