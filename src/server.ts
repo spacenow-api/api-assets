c58b5cbc87d20855e36205b0a492b7da5ad0dffc
Lucas <csalucasnascimento@gmail.com>
2019-07-29 10:40:31 +1000

    removing assets

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,14 @@
-import { PORT } from './config';
+import { PORT } from "./config";
 
-import App from './App';
+import App from "./App";
 
-// import AssetsController from "./controllers/assets/asset.controller";
-import HealthController from './controllers/health/health.controller';
-import LegacyPhotosController from './controllers/legacy/legacyPhotos.controller';
+import HealthController from "./controllers/health/health.controller";
+import LegacyPhotosController from "./controllers/legacy/legacyPhotos.controller";
 
 const app = new App(
-	[
-		// new AssetsController(),
-		new HealthController(),
-		new LegacyPhotosController(),
-	],
-	PORT,
-	'0.0.0.0',
+  [new HealthController(), new LegacyPhotosController()],
+  PORT,
+  "0.0.0.0"
 );
 
 app.listen();
