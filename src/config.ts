63ca2a34adde945109d0445c8df0ed27a508b3d8
Arthemus <arthemus.moreira@gmail.com>
2019-08-09 14:15:00 +1000

    New service to send email.

--- src/config.ts
+++ src/config.ts
@@ -14,5 +14,15 @@ export const dbUsername = process.env.DATABASE_USERNAME;
 export const dbPassword = process.env.DATABASE_PASSWORD;
 export const dbHost = process.env.DATABASE_HOST;
 
-export const USERS_AUTHENTICATION_API_HOST =
-  process.env.USERS_API_HOST || "http://localhost:6001";
+export const USERS_AUTHENTICATION_API_HOST = process.env.USERS_API_HOST || "http://localhost:6001";
+
+export const emailConfig = {
+  host: 'smtp.gmail.com',
+  port: 587,
+  email: 'no-reply@spacenow.com',
+  sender: 'Spacenow No-Reply',
+  senderEmail: 'no-reply@spacenow.com',
+  password: process.env.SMTP_LOGIN_PASSWORD,
+  secure: false,
+  tls: true,
+};
--- src/controllers/mail/mail.controller.ts
+++ src/controllers/mail/mail.controller.ts
@@ -1,6 +1,10 @@
-import { Router, Request, Response, NextFunction } from "express";
+import { Router, Request, Response, NextFunction } from 'express';
+import nodemailer from 'nodemailer';
 
-import errorMiddleware from "../../helpers/middlewares/error-middleware";
+import errorMiddleware from '../../helpers/middlewares/error-middleware';
+import { IMailRequest } from '../../interfaces/mail.interface';
+
+import { DEBUG, emailConfig } from './../../config';
 
 class MailController {
 
@@ -15,11 +19,49 @@ class MailController {
   }
 
   private onSendMail = async (req: Request, res: Response, next: NextFunction) => {
+    let transporter;
     try {
+      const data = <IMailRequest>req.body;
+      // TODO: Need to define host, user and pass for real environment...
+      let host = emailConfig.host;
+      let port = emailConfig.port;
+      let from = emailConfig.email;
+      let username = emailConfig.email;
+      let password = emailConfig.password;
+      let tls = emailConfig.tls;
+      if (DEBUG) {
+        const testAccount = await nodemailer.createTestAccount();
+        host = testAccount.smtp.host;
+        port = testAccount.smtp.port;
+        from = 'example@nodemailer.com';
+        username = testAccount.user;
+        password = testAccount.pass;
+        tls = false;
+      }
+      transporter = nodemailer.createTransport({
+        logger: DEBUG,
+        host: host,
+        port: port,
+        secure: false,
+        auth: {
+          user: username,
+          pass: password
+        },
+        tls: {
+          rejectUnauthorized: tls,
+        }
+      });
+      const info = await transporter.sendMail({ ...data, from });
+      console.info('Message sent: %s', info.messageId);
+      if (DEBUG)
+        console.warn('Preview URL: %s', nodemailer.getTestMessageUrl(info));
       res.end();
     } catch (err) {
       console.error(err);
       errorMiddleware(err, req, res, next);
+    } finally {
+      if (transporter)
+        transporter.close();
     }
   };
 }
