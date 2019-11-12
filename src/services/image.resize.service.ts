e2bff75b4e969a1c19633d5c366110fb91fbf807
Arthemus <arthemus.moreira@gmail.com>
2019-11-12 15:22:32 +1100

    Removing lib to compress images.

--- src/services/image.resize.service.ts
+++ src/services/image.resize.service.ts
@@ -1,27 +1,13 @@
 import sharp from "sharp";
 import axios from "axios";
-import imagemin from 'imagemin'
-import imageminPngquant from 'imagemin-pngquant'
-import imageminJpegtran from 'imagemin-jpegtran'
 
 const resize = async (path: string, width?: number, height?: number) => {
   const fileResponse = await axios({ url: path, method: "GET", responseType: "arraybuffer" });
   const buffer = Buffer.from(fileResponse.data, "base64");
-
-  // Compression...
-  const bufferResult = await imagemin.buffer(buffer, {
-    plugins: [
-      imageminJpegtran(),
-      imageminPngquant({ quality: [0.3, 0.5] })
-    ]
-  });
-
-  // Crop and formatting...
-  const transform = sharp(bufferResult).toFormat("jpeg");
+  const transform = sharp(buffer).toFormat("jpeg");
   if (width || height) {
     transform.resize(width, height);
   }
-
   return transform.toBuffer();
 }
 
