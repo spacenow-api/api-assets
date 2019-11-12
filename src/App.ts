587816c13b21f3aceebe64ea76f8c9f7e94166e8
Arthemus <arthemus.moreira@gmail.com>
2019-11-13 10:43:10 +1100

    Adding compression.

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
 import express, { Application } from "express";
+import compression from 'compression';
 import cookieParse from "cookie-parser";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -35,6 +36,7 @@ class App {
     this.app.use(loggerMiddleware);
     this.app.use(bodyParser.json());
     this.app.use(cookieParse());
+    this.app.use(compression());
   }
 
   private initializeErrorHandling(): void {
